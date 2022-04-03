888030724ca64c06ea1ae86285c58490f1928b64
nohorseKnight <474610708@qq.com>
2022-04-04 02:28:57 +0800

    元素攻击功能， tips基础功能

--- Assets/Scripts/Controller/Cricle.cs
+++ Assets/Scripts/Controller/Cricle.cs
@@ -14,17 +14,6 @@ namespace QFramework.Example
     public class Cricle : BaseController
     {
         [SerializeField] public CircleNumer CurrentCircleNumer;
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
 
         void OnTriggerEnter2D(Collider2D other)
         {
